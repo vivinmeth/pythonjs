9bbc0340b368d0308f0b24a84adb5981b27b9fef
vivinmeth <vivinmeth@gmail.com>
2021-01-07 15:53:26 +0530

    added new urls to release

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PythonJS",
-    version="0.0.8",
+    version="0.0.81",
     author="VivinMeth L V",
     author_email="vivinmeth@gmail.com",
     description="JS Features [like Object, Map] in python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="BSD-3-Clause",
-    url="https://github.com/vivinmeth/pythonjs",
+    url="https://gitlab.com/vivinmeth/pythonjs",
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
@@ -26,6 +26,6 @@ setuptools.setup(
         # 'Funding': 'https://donate.pypi.org',
         # 'Say Thanks!': 'http://saythanks.io/to/example',
         'Source': 'https://github.com/vivinmeth/pythonjs',
-        'Tracker': 'https://github.com/vivinmeth/pythonjs/issues',
+        'Tracker': 'https://gitlab.com/vivinmeth/pythonjs/issues',
     },
 )
